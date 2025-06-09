77df8d2cff64fb42ff660a1d655b173de3e8ca42
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2025-06-09 13:04:46 +0100

    Update to Frontend
    - Made Mockup of design
    - Added Favicon

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ if (require('electron-squirrel-startup')) {
 }
 
 const createWindow = () => {
+  const iconPath = path.join(__dirname, 'icons', 'favicon.ico'); 
+
+  console.debug(`ICON PATH : ${iconPath}`)
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: iconPath,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
